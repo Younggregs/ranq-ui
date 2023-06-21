3bc1da6896484d4ea4c4cc9946ddc8891a9418e3
retzam <retzam@bento.africa>
2023-06-21 22:07:13 +0100

    update

--- app/lib/constants.tsx
+++ app/lib/constants.tsx
@@ -5,7 +5,7 @@ const cardWidth = "30ch"
 // const uiUrl = "http://localhost:3000"
 
 // prod
-const url = "https://api.ieuautomation.online:8000/graphql"
+const url = "https://api.ieuautomation.online/graphql"
 const uiUrl = "http://ranq-ui.vercel.app/"
 
 export {
