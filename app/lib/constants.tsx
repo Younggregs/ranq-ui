544614432220c2a71f420ee15bf42ae6565f687a
retzam <retzam@bento.africa>
2023-06-21 21:56:26 +0100

    update url to https

--- app/lib/constants.tsx
+++ app/lib/constants.tsx
@@ -5,7 +5,7 @@ const cardWidth = "30ch"
 // const uiUrl = "http://localhost:3000"
 
 // prod
-const url = "http://144.126.223.174:8000/graphql"
+const url = "https://api.ieuautomation.online:8000/graphql"
 const uiUrl = "http://ranq-ui.vercel.app/"
 
 export {
